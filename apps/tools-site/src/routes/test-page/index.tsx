0b543a138ecff04ec22ed79870c1f58f5e0f9777
SpiritLing <spiritling@outlook.com>
2023-08-07 10:59:37 +0900

    fix(moaco): add dynamic theme

--- apps/tools-site/src/routes/test-page/index.tsx
+++ apps/tools-site/src/routes/test-page/index.tsx
@@ -1,23 +1,21 @@
 import { useRef } from "react";
 import Editor from "@monaco-editor/react";
-import { Button } from "antd";
+import { useAppSelector } from "@project-self/store/store";
+import { selectGlobalState } from "@project-self/store/selector";
 
 const TestPage = () => {
+	const globalState = useAppSelector(selectGlobalState);
 	const editorRef = useRef(null);
 	const handleEditorDidMount = (editor: any, monaco: any) => {
 		editorRef.current = editor;
 	};
-	const showValue = () => {
-		if (editorRef.current != null) {
-			alert((editorRef.current as any).getValue());
-		}
-	};
 	return (
 		<section className={"overflow-hidden m-0 p-0"}>
-			<Button onClick={showValue}>获取值</Button>
 			<Editor
 				onMount={handleEditorDidMount}
-				height="100%"
+				height={"100%"}
+				width={"100%"}
+				theme={globalState.theme.isDark ? "vs-dark" : "light"}
 				defaultLanguage="markdown"
 				defaultValue="// some comment"
 			/>
