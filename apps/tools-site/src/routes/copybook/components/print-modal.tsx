3bbbb1e7f61ce765a0c82b3229443ea73d96ce93
SpiritLing <spiritling@outlook.com>
2023-11-01 21:58:49 +0800

    finsih print, a litter optimize

--- apps/tools-site/src/routes/copybook/components/print-modal.tsx
+++ apps/tools-site/src/routes/copybook/components/print-modal.tsx
@@ -1,17 +1,22 @@
-import { Modal } from "antd";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Button, Modal } from "antd";
+import { Dispatch, ReactInstance, SetStateAction, useRef, useState } from "react";
+import ZiTieBg from "@project-self/assets/bg.svg";
+import { useReactToPrint } from "react-to-print";
 
 const parserString = (str: string): string[] => {
 	const strArr = str.replaceAll("\r\n", "\n").replaceAll("\r", "\n").split("\n");
-	const newStrArr = [];
+	let newStrArr: string[] = [];
 	for (let i = 0; i < strArr.length; i++) {
 		const temp1 = [...strArr[i]];
-		temp1.unshift(" ", " ");
+		temp1.unshift("-", "-");
 		const shengyu = temp1.length % 12;
-		for (let j = 0; j < shengyu; j++) {
-			temp1.push(" ");
+		if (shengyu != 0) {
+			for (let j = 0; j < 12 - shengyu; j++) {
+				temp1.push("-");
+			}
 		}
-		newStrArr.push(temp1.join(""));
+
+		newStrArr = newStrArr.concat(temp1);
 	}
 	return newStrArr;
 };
@@ -20,7 +25,20 @@ const PrintModal = (props: {
 	content: string;
 	open: boolean;
 	setOpen: Dispatch<SetStateAction<boolean>>;
+	fontFamily: string;
 }) => {
+	const componentRef = useRef(null);
+	const handlePrint = useReactToPrint({
+		content: () => componentRef.current as unknown as ReactInstance,
+		copyStyles: true,
+		documentTitle: "字帖打印",
+		fonts: [
+			{
+				family: props.fontFamily,
+				source: props.fontFamily,
+			},
+		],
+	});
 	const contentArr = parserString(props.content);
 	console.log(contentArr);
 	return (
@@ -28,12 +46,41 @@ const PrintModal = (props: {
 			title="Modal"
 			centered
 			open={props.open}
-			onOk={() => props.setOpen(false)}
+			onOk={handlePrint}
 			onCancel={() => props.setOpen(false)}
+			okText={"打印"}
 			width={1000}
 			destroyOnClose={true}
 		>
-			<p>{props.content}</p>
+			<ul
+				ref={componentRef}
+				style={{
+					padding: "0",
+					width: "940px",
+					margin: "10px auto",
+					fontFamily: props.fontFamily,
+				}}
+			>
+				{contentArr.map((x, index) => (
+					<li
+						key={index}
+						style={{
+							display: "inline-block",
+							backgroundImage: `url(${ZiTieBg})`,
+							width: "80px",
+							height: "80px",
+							color: "rgb(184, 184, 184)",
+							fontFamily: props.fontFamily,
+							fontSize: "58px",
+							textAlign: "center",
+							lineHeight: "80px",
+							margin: "5px 0px 5px -2px",
+						}}
+					>
+						{x}
+					</li>
+				))}
+			</ul>
 		</Modal>
 	);
 };
--- apps/tools-site/src/routes/copybook/index.tsx
+++ apps/tools-site/src/routes/copybook/index.tsx
@@ -1,15 +1,23 @@
 import { Button, Input } from "antd";
 import PrintModal from "./components/print-modal";
 import { useState } from "react";
+import value from "./test.txt?raw";
 
 const CopyBook = () => {
 	const [open, setOpen] = useState(false);
-	const [content, setContent] = useState("");
+	const [content, setContent] = useState(value);
+	const [fontFamily, setFontFamily] = useState(`"方正行楷细 简", FZXingKaiXiS`);
 	return (
 		<section>
-			<Input.TextArea rows={10} onChange={(e) => setContent(e.target.value)}></Input.TextArea>
+			<p>Edge无法读取系统字体，Chrome可以的</p>
+			<Input.TextArea
+				rows={10}
+				value={content}
+				onChange={(e) => setContent(e.target.value)}
+			></Input.TextArea>
+			<Input value={fontFamily} onChange={(e) => setFontFamily(e.target.value)} />
 			<Button onClick={() => setOpen(true)}>生成字帖</Button>
-			<PrintModal content={content} open={open} setOpen={setOpen} />
+			<PrintModal content={content} open={open} setOpen={setOpen} fontFamily={fontFamily} />
 		</section>
 	);
 };
