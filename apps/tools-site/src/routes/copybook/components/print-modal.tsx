615ed6bf60a4ddaac0ebbf5a9f70b0fb71c75d05
SpiritLing <spiritling@outlook.com>
2023-11-02 11:22:10 +0900

    feat(copybook): fix style

--- apps/tools-site/src/routes/copybook/components/print-modal.tsx
+++ apps/tools-site/src/routes/copybook/components/print-modal.tsx
@@ -1,24 +1,48 @@
 import { Button, Modal } from "antd";
-import { Dispatch, ReactInstance, SetStateAction, useRef, useState } from "react";
+import React, { Dispatch, ReactInstance, SetStateAction, useRef, useState } from "react";
 import ZiTieBg from "@project-self/assets/bg.svg";
 import { useReactToPrint } from "react-to-print";
+import styles from "./print-modal.module.scss";
 
-const parserString = (str: string): string[] => {
-	const strArr = str.replaceAll("\r\n", "\n").replaceAll("\r", "\n").split("\n");
+const paragraphFormatted = (paragraphs: string[], wordsPerLine: number) => {
 	let newStrArr: string[] = [];
-	for (let i = 0; i < strArr.length; i++) {
-		const temp1 = [...strArr[i]];
-		temp1.unshift("-", "-");
-		const shengyu = temp1.length % 12;
-		if (shengyu != 0) {
-			for (let j = 0; j < 12 - shengyu; j++) {
-				temp1.push("-");
+	for (let i = 0; i < paragraphs.length; i++) {
+		const temp1 = [...paragraphs[i]];
+		temp1.unshift(" ", " ");
+		const excess = temp1.length % wordsPerLine;
+		if (excess != 0) {
+			for (let j = 0; j < wordsPerLine - excess; j++) {
+				temp1.push(" ");
 			}
 		}
 
 		newStrArr = newStrArr.concat(temp1);
 	}
 	return newStrArr;
+};
+
+/**
+ *
+ * @param text 文本
+ * @param linesPerSegment 多少行分割
+ * @param wordsPerLine 每行字数
+ */
+const textFormatted = (text: string, linesPerSegment: number, wordsPerLine: number): string[][] => {
+	const paragraphList = text.replaceAll("\r\n", "\n").replaceAll("\r", "\n").split("\n");
+	const textArr: string[] = paragraphFormatted(paragraphList, wordsPerLine);
+	const segments: string[][] = [];
+	const sum = linesPerSegment * wordsPerLine;
+	for (let i = 0; i < textArr.length; i += sum) {
+		const segmentLines = textArr.slice(i, i + sum);
+		if (segmentLines.length < sum) {
+			const excessCount = segmentLines.length;
+			for (let j = 0; j < excessCount; j++) {
+				segmentLines.push(" ");
+			}
+		}
+		segments.push(segmentLines);
+	}
+	return segments;
 };
 
 const PrintModal = (props: {
@@ -35,12 +59,48 @@ const PrintModal = (props: {
 		fonts: [
 			{
 				family: props.fontFamily,
-				source: props.fontFamily,
+				source: `local(props.fontFamily)`,
 			},
 		],
 	});
-	const contentArr = parserString(props.content);
-	console.log(contentArr);
+	const contentArr = textFormatted(props.content, 15, 12);
+	// 渲染段落和行
+	const renderedSegments = (segments: string[][]) => {
+		return segments.map((segment, index) => (
+			<>
+				<div className={styles.ziPage}>
+					<ul key={index} className={styles.ziUl}>
+						{segment.map((line, lineIndex) => (
+							<li
+								key={lineIndex}
+								style={{
+									display: "inline-block",
+									backgroundImage: `url(${ZiTieBg})`,
+									width: "80px",
+									height: "80px",
+									color: "rgb(184, 184, 184)",
+									fontFamily: props.fontFamily,
+									fontSize: "58px",
+									textAlign: "center",
+									lineHeight: "80px",
+									margin: "5px 0px 5px -2px",
+								}}
+							>
+								{line === " " ? "\u00A0" : line}
+							</li>
+						))}
+					</ul>
+				</div>
+				{index == segments.length - 1 || (
+					<div className={styles.afterPage}>
+						<div className={styles.pageHeader} style={{ color: "rgb(102, 102, 102)" }}>
+							{"  \u000C  "}
+						</div>
+					</div>
+				)}
+			</>
+		));
+	};
 	return (
 		<Modal
 			title="Modal"
@@ -52,35 +112,10 @@ const PrintModal = (props: {
 			width={1000}
 			destroyOnClose={true}
 		>
-			<ul
-				ref={componentRef}
-				style={{
-					padding: "0",
-					width: "940px",
-					margin: "10px auto",
-					fontFamily: props.fontFamily,
-				}}
-			>
-				{contentArr.map((x, index) => (
-					<li
-						key={index}
-						style={{
-							display: "inline-block",
-							backgroundImage: `url(${ZiTieBg})`,
-							width: "80px",
-							height: "80px",
-							color: "rgb(184, 184, 184)",
-							fontFamily: props.fontFamily,
-							fontSize: "58px",
-							textAlign: "center",
-							lineHeight: "80px",
-							margin: "5px 0px 5px -2px",
-						}}
-					>
-						{x}
-					</li>
-				))}
-			</ul>
+			<div ref={componentRef}>
+				<div className={styles.pageHeader} style={{ color: "rgb(102, 102, 102)" }}></div>
+				{renderedSegments(contentArr)}
+			</div>
 		</Modal>
 	);
 };
